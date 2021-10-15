d0153872904f66bdf8defeb7d090dc4bf0c95608
Rong zhu(FB) <rong.zhu@dena.com>
2021-10-15 13:24:44 +0800

    Add docker entrypoint

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [ "x$1 = "xmaster" ]; then
+    while true; do
+        echo master running
+        sleep 30
+    done
+fi
+$JMETER_HOME/bin/jmeter-server \
+    -Dserver.rmi.localport=50000 \
+    -Dserver_port=1099 \
+    -Jserver.rmi.ssl.disable=true
